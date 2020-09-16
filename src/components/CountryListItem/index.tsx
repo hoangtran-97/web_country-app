6e75e10551dc074798da62e31df103e5e28146b2
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-09-16 12:59:52 +0300

    feat: restyle table

--- src/components/CountryListItem/index.tsx
+++ src/components/CountryListItem/index.tsx
@@ -6,26 +6,40 @@ import styles from "./CountryListItem.module.css";
 export const CountryListItem = ({ country, width }: CountryListItemProps) => {
     const { name, flag, languages, region, population } = country;
     return (
-        <tr className={styles.container}>
-            <td>
-                <img alt="flag" src={flag} className={styles.flag}></img>
-            </td>
-
+        <>
             {width <= 900 ? (
-                <td className={styles.text__container}>
-                    <p>{name}</p>
-                    <p>{languages[0].name}</p>
-                    <p>{population}</p>
-                    <p>{region}</p>
-                </td>
-            ) : (
                 <>
+                    <tr>
+                        <td colSpan={4}>
+                            <img
+                                alt="flag"
+                                src={flag}
+                                className={styles.flag}
+                            ></img>
+                        </td>
+                    </tr>
+                    <tr className={styles.text__container}>
+                        <td>{name}</td>
+                        <td>{languages[0].name}</td>
+                        <td>{population}</td>
+                        <td>{region}</td>
+                    </tr>
+                </>
+            ) : (
+                <tr className={styles.container}>
+                    <td>
+                        <img
+                            alt="flag"
+                            src={flag}
+                            className={styles.flag}
+                        ></img>
+                    </td>
                     <td>{name}</td>
                     <td>{languages[0].name}</td>
                     <td>{population}</td>
                     <td>{region}</td>
-                </>
+                </tr>
             )}
-        </tr>
+        </>
     );
 };
