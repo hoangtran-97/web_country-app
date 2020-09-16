347fc2587448594d4574528534dbcaeab26c1b73
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-09-16 13:31:57 +0300

    feat: mobile buy UI

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 
 import { SearchProps } from "../../typings";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 import styles from "./Search.module.css";
 
 export const Search = React.memo(
     ({ placeholder, query, setQuery }: SearchProps) => {
         return (
-            <input
-                className={styles.search}
-                placeholder={placeholder}
-                value={query}
-                onChange={event => setQuery(event.target.value)}
-            ></input>
+            <div className={styles.container}>
+                <AiOutlineSearch
+                    className={styles.icon__search}
+                ></AiOutlineSearch>
+                <input
+                    className={styles.search}
+                    placeholder={placeholder}
+                    value={query}
+                    onChange={event => setQuery(event.target.value)}
+                ></input>
+                <AiOutlineClose className={styles.icon__close}></AiOutlineClose>
+            </div>
         );
     }
 );
